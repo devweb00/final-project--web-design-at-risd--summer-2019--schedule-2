701919bc34deb0c947619a1ca58ce7982712ff37
Grady Jason Ng <gradyjasonng@gmail.com>
2019-07-19 11:53:23 -0400

    Content updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 
+var initbgcol = $(".sub-nav li:nth-child(1)").attr("data-bg-col")
+ $("html").css("background", initbgcol);
+ $(".navbar").css({"background": initbgcol, "box-shadow": "0 -60px 60px " + initbgcol});
+
  var cutoffValues = [];
    $(".portfolio-item").each(function(){
       cutoffValues.push($(this).offset().top + $(this).height() - 300);
